a9122188979c7bb4cf556ef44ba97bcb5c7a40aa
shubhamPrakashJha <sjonline4u@gmail.com>
2017-09-11 09:11:31 +0530

    added personal message

--- decoder.py
+++ decoder.py
@@ -1,14 +1,15 @@
 import os
 def rename_file():
     #take all file name in directory
-    file_list = os.listdir(r"C:\Users\Shubham Prakash\Desktop\udacity\prank")
+    file_list = os.listdir(r"C:\Users\Shubham Prakash\Desktop\6Anniversery\surprise")
     #print(file_list)
 
     #rename all those file name
-    os.chdir(r"C:\Users\Shubham Prakash\Desktop\udacity\prank")
+    os.chdir(r"C:\Users\Shubham Prakash\Desktop\6Anniversery\surprise"
+             r"")
     for file_name in file_list:
         print("old name = "+file_name)
         print("new name = "+ file_name.strip('0123456789'))
         print("=========================================")
-        os.rename(file_name,file_name.strip('0123456789'))
+        os.rename(file_name,file_name.strip('()0123456789'))
 rename_file()
